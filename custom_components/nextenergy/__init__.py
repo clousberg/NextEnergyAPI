df15cda222422e66ab32e82db2d60829027de860
ServError <1589596+ServError@users.noreply.github.com>
2025-11-29 02:09:07 +0100

    Fix deprecated usage of async_forward_entry_setup.
    https://developers.home-assistant.io/blog/2024/06/12/async_forward_entry_setups/

--- custom_components/nextenergy/__init__.py
+++ custom_components/nextenergy/__init__.py
@@ -1,18 +1,17 @@
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from .const import DOMAIN, PLATFORMS
+from .const import DOMAIN
+
+PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
     # Forward to sensor platform
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, platform)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    for platform in PLATFORMS:
-        await hass.config_entries.async_forward_entry_unload(entry, platform)
+    await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     hass.data[DOMAIN].pop(entry.entry_id, None)
     return True
--- custom_components/nextenergy/const.py
+++ custom_components/nextenergy/const.py
@@ -1,4 +1,3 @@
 DOMAIN = "nextenergy"
-PLATFORMS = ["sensor"]
 API_URL = "https://mijn.nextenergy.nl/Mobile_EnergyNext/MarketPrices"
 DEFAULT_UPDATE_INTERVAL = 3600  # seconds
