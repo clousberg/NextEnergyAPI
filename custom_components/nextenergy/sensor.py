a2c8ef7342dd6550ba8b1806903b7395e91ce5bb
clousberg <87444372+clousberg@users.noreply.github.com>
2025-12-31 10:56:50 +0100

    Refactor NextEnergyPriceSensor to use CoordinatorEntity

--- custom_components/nextenergy/sensor.py
+++ custom_components/nextenergy/sensor.py
@@ -1,8 +1,10 @@
 from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from .const import DOMAIN
 
 async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data[DOMAIN][entry.entry_id]
+
     async_add_entities([
         NextEnergyPriceSensor(coordinator, "current_market"),
         NextEnergyPriceSensor(coordinator, "current_market_plus"),
@@ -10,22 +12,18 @@ async def async_setup_entry(hass, entry, async_add_entities):
         NextEnergyPriceSensor(coordinator, "next_market_plus"),
     ])
 
-class NextEnergyPriceSensor(SensorEntity):
+
+class NextEnergyPriceSensor(CoordinatorEntity, SensorEntity):
     def __init__(self, coordinator, key):
-        self.coordinator = coordinator
+        super().__init__(coordinator)
         self.key = key
         self._attr_name = f"NextEnergy {key.replace('_', ' ').title()}"
         self._attr_unique_id = f"nextenergy_{key}"
 
     @property
-    def state(self):
-        data = self.coordinator.data or {}
-        return round(data.get(self.key, 0), 4)
+    def native_value(self):
+        return round(self.coordinator.data.get(self.key, 0), 4)
 
     @property
-    def unit_of_measurement(self):
+    def native_unit_of_measurement(self):
         return "€/kWh"
-
-    @property
-    def available(self):
-        return self.coordinator.last_update_success
